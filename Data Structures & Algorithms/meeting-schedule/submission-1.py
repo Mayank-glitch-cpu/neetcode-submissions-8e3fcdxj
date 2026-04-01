a0412dfe8655d98e92bfccd721b93d0e70fd04e1
Mayank Vyas <mvyas7@asu.edu>
2026-03-31 19:12:42 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x:x.start)
+        end=  intervals[0].end
+
+        for interval in intervals[1:]:
+            if end <= interval.start:
+                end= interval.end
+            else:
+                return False
+        return True
