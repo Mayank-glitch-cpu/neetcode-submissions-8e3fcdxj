5461fc35bb43441360c3346f6896a76e79528c7c
Mayank Vyas <mvyas7@asu.edu>
2026-04-14 11:42:19 -0700

    Add: plus-one - submission-3

---
+++ Data Structures & Algorithms/plus-one/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry=False
+        if digits[-1] < 9:
+            digits[-1]+=1
+            return digits
+        else:
+            carry=True
+            for i in range(len(digits)-2,-1,-1):
+                digits[i+1]=0
+                # print('with carry True',digits)
+                if digits[i]<9:
+                    digits[i]+=1
+                    carry= False
+                    # print('with carry false',digits)
+                    break
+        if carry:
+            digits[0]=0
+            digits.insert(0,1)
+            return digits
+        return digits
