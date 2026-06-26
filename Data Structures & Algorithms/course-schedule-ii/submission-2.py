db98dff38b91ef467d0e89698ec92a3e6b356202
Mayank Vyas <mvyas7@asu.edu>
2026-06-26 09:40:29 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        n= numCourses
+        topo=[]
+        adj = {i:[] for i in range(n)}
+        indegree= [0]*n
+
+        for crs, pre in prerequisites:
+            adj[pre].append(crs)
+            indegree[crs]+=1
+        # print(indegree)
+        q= deque()
+
+        for i in range(n):
+            if indegree[i]==0:
+                q.append(i)
+        # cnt=0
+        while q:
+            crs= q.popleft()
+            topo.append(crs)
+            # cnt+=1
+            for pre in adj[crs]:
+                indegree[pre]-=1
+                if indegree[pre]==0:
+                    q.append(pre)
+        # print(cnt)
+        # print(topo)
+        return topo if len(topo)==n else []
